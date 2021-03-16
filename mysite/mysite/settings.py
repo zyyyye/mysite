79512114e119c73283e2275a44533a1501f49b79
zyyyye <zyyyye@qq.com>
2021-03-16 15:21:09 +0800

    最终版

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -85,9 +85,17 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
+    'default' : {
+        'ENGINE' : 'django.db.backends.mysql',
+        'NAME' : 'django_mysql',
+        'USER' : 'zyyyye',
+        'PASSWORD' : 'zyyyye',
+        'HOST' : '127.0.0.1',
+        'PORT' : '3306',
     }
 }
 
