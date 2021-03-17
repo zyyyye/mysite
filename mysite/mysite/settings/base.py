20c61502d2b291bed9028266b992d794b011b9c4
zyyyye <zyyyye@qq.com>
2021-03-17 12:23:33 +0800

    部署配置

--- mysite/mysite/settings/base.py
+++ mysite/mysite/settings/base.py
@@ -14,19 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0$8l!%vykwg@x^gf$h^#0r(pa^qscs9gch8&*t!l235rrs!@$z'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Application definition
@@ -79,25 +67,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # },
-    'default' : {
-        'ENGINE' : 'django.db.backends.mysql',
-        'NAME' : 'django_mysql',
-        'USER' : 'zyyyye',
-        'PASSWORD' : 'zyyyye',
-        'HOST' : '127.0.0.1',
-        'PORT' : '3306',
-    }
-}
 
 
 # Password validation
@@ -174,11 +143,3 @@ CACHES = {
         'LOCATION' : 'my_cache_table',
     }
 }
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.qq.com'
-EMAIL_PORT = 465
-EMAIL_HOST_USER = 'zyyyye@qq.com'
-EMAIL_HOST_PASSWORD = 'hgfzwjsfblysdfjh'
-EMAIL_SUBJECT_PREFIX = '[my_blog]'
-EMAIL_USE_SSL = True
